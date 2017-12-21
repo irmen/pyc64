42aa731923dc7863ee4474152eaf8f9d9a8bb90b
Irmen de Jong <irmen@razorvine.net>
2017-12-21 15:13:49 +0100

    il65 moved to separate repo

--- il65/il65/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Intermediate Language for 6502/6510 microprocessors
-
-Written by Irmen de Jong (irmen@razorvine.net)
-License: GNU GPL 3.0, see LICENSE
-"""
--- il65/il65/__main__.py
+++
@@ -1,9 +0,0 @@
-"""
-Intermediate Language for 6502/6510 microprocessors
-
-Written by Irmen de Jong (irmen@razorvine.net)
-License: GNU GPL 3.0, see LICENSE
-"""
-
-from . import il65
-il65.main()
